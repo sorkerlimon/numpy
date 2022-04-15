8b3df3de93f5dee79c820fa1fc4d796319a100de
SorkerLimon <sorkerlimon18@gmail.com>
2022-04-15 09:20:00 +0600

    Panda code added more

--- panda.py
+++ panda.py
@@ -2,11 +2,22 @@ import numpy as np
 import pandas as pd
 
 data = np.arange(5000).reshape(100,50)
-print(data)
+# print(data)
 columName = ["Column_"+str(number) for number in range(50)]
-print(columName)
+# print(columName)
+#
+# sampleData = pd.DataFrame(data) ### numpy data added into panda data frame
+# print(sampleData)
+#
+# head = sampleData.head()     ##### First 5 line show
+# print(head)
+#
+# datatype = type(sampleData)  ### Data type
+# print(datatype)
+#
+# firstIndex = sampleData[0]    ## Index acces
+# print(firstIndex)
 
-sampleData = pd.DataFrame(data)
-print(sampleData)
-
+columNameadded = pd.DataFrame(data,columns=columName)
+print(columNameadded)
 
