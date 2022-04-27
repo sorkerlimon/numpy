c8a4489e5207bc2662b32aa67f20aa969ecd020f
SorkerLimon <sorkerlimon18@gmail.com>
2022-04-27 13:24:27 +0600

    panda code added

--- panda.py
+++ panda.py
@@ -158,8 +158,8 @@ data = pd.read_csv("C:/Users/21100002/Desktop/Pandas/Indian Data Set/indian_live
 
 
 ### Apply with Lambda
-data = pd.read_csv("C:/Users/21100002/Desktop/Pandas/Indian Data Set/indian_liver_patient.csv")
-print(data.head())
+# data = pd.read_csv("C:/Users/21100002/Desktop/Pandas/Indian Data Set/indian_liver_patient.csv")
+# print(data.head())
 # print(data["Total_Bilirubin"])
 #
 # def checkfun(x):
@@ -200,3 +200,19 @@ print(data.head())
 
 
 ########################### Merge , join ,Concate , Append in pandas
+
+table1 = pd.DataFrame(data={'column1': [1, 2, 3, 4, 5], 'column2': [6, 7, 8, 9, 10]})
+table2 = pd.DataFrame(data={'column1': [11, 12, 13, 14, 15], 'column2': [16, 17, 18, 19, 20]})
+table3 = pd.DataFrame(data={'column3': [1, 1, 3, 4, 5], 'column4': [6, 7, 8, 9, 10]})
+
+
+caoncantation = pd.concat([table1,table2])
+# print(caoncantation)
+
+caoncantation1 = pd.concat([table1,table3],axis=1)
+# print(caoncantation1) 
+
+caoncantation1["different"] = caoncantation1["column4"]- caoncantation1["column1"]
+print(caoncantation1)
+
+
